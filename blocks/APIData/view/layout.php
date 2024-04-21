8feaa361c6652707fae875ea35302143d2d74706
Yurii Pavlov <mailyurik@gmail.com>
2024-04-21 23:06:51 +0200

    add table styles

--- blocks/APIData/view/layout.php
+++ blocks/APIData/view/layout.php
@@ -16,33 +16,36 @@ $data = $data ?? [];
     <?php if (!empty($data['apiData']->title)) { ?>
         <h2><?php echo esc_html($data['apiData']->title); ?></h2>
     <?php } ?>
-    <table>
-        <tr>
-            <?php foreach ($data['columns'] as $column) {
-                if (isset($data['apiData']->data->headers[$column])) { ?>
-                    <th><?php echo esc_html($data['apiData']->data->headers[$column]); ?> </th>
-                <?php }
-            } ?>
-        </tr>
-
-        <?php if (!empty($data['apiData']->data->rows)) { ?>
-            <?php foreach ($data['apiData']->data->rows as $row) { ?>
-                <tr>
-                    <?php $index = 0; ?>
-                    <?php foreach ($row as $colIndex => $colData) { ?>
-                        <?php if (in_array($index, $data['columns'])) { ?>
-                            <td>
-                                <?php if ($colIndex === 'date') {
-                                    echo date('Y-m-d H:i:s', esc_html($colData));
-                                } else {
-                                    echo esc_html($colData);
-                                } ?>
-                            </td>
+    <table class="table table-striped table-hover">
+        <thead class="thead-dark">
+            <tr>
+                <?php foreach ($data['columns'] as $column) {
+                    if (isset($data['apiData']->data->headers[$column])) { ?>
+                        <th scope="col"><?php echo esc_html($data['apiData']->data->headers[$column]); ?> </th>
+                    <?php }
+                } ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if (!empty($data['apiData']->data->rows)) { ?>
+                <?php foreach ($data['apiData']->data->rows as $row) { ?>
+                    <tr>
+                        <?php $index = 0; ?>
+                        <?php foreach ($row as $colIndex => $colData) { ?>
+                            <?php if (in_array($index, $data['columns'])) { ?>
+                                <td>
+                                    <?php if ($colIndex === 'date') {
+                                        echo date('Y-m-d H:i:s', esc_html($colData));
+                                    } else {
+                                        echo esc_html($colData);
+                                    } ?>
+                                </td>
+                            <?php } ?>
+                            <?php $index++; ?>
                         <?php } ?>
-                        <?php $index++; ?>
-                    <?php } ?>
-                </tr>
+                    </tr>
+                <?php } ?>
             <?php } ?>
-        <?php } ?>
+        </tbody>
     </table>
 </div>
